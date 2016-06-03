f646572bbee0c5f00f3d59f1d367a521854060b5
Budi Pratama <boedipratama19@gmail.com>
2016-06-03 22:14:32 +0700

    navigasi baru

--- application/views/moltran/components/v-backend-navigation.php
+++ application/views/moltran/components/v-backend-navigation.php
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
-=======
- Navigation Bar-->
->>>>>>> 82605b4a56ab3483eea792247272805111c7e603
-        <header id="topnav">
+<header id="topnav">
             <div class="topbar-main">
                 <div class="container">
 
                     <!-- LOGO -->
                     <div class="topbar-left">
-                        <a href="index.html" class="logo"><img height="40" src="<?php echo $params['base_image'] . 'favicon.png'; ?>" />&nbsp; <span>Plasa Telkom </span></a>
+                        <a href="index.html" class="logo"><i class="md md-terrain"></i> <span>Moltran </span></a>
                     </div>
                     <!-- End Logo container-->
 
@@ -16,11 +12,73 @@
                     <div class="menu-extras">
 
                         <ul class="nav navbar-nav navbar-right pull-right">
-                            
+                            <li>
+                                <form role="search" class="navbar-left app-search pull-left hidden-xs">
+                                     <input type="text" placeholder="Search..." class="form-control">
+                                     <a href=""><i class="fa fa-search"></i></a>
+                                </form>
+                            </li>
+                            <li class="dropdown hidden-xs">
+                                <a href="#" data-target="#" class="dropdown-toggle waves-effect waves-light" data-toggle="dropdown" aria-expanded="true">
+                                    <i class="md md-notifications"></i> <span class="badge badge-xs badge-danger">3</span>
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-lg">
+                                    <li class="text-center notifi-title">Notification</li>
+                                    <li class="list-group">
+                                       <!-- list item-->
+                                       <a href="javascript:void(0);" class="list-group-item">
+                                          <div class="media">
+                                             <div class="pull-left">
+                                                <em class="fa fa-user-plus fa-2x text-info"></em>
+                                             </div>
+                                             <div class="media-body clearfix">
+                                                <div class="media-heading">New user registered</div>
+                                                <p class="m-0">
+                                                   <small>You have 10 unread messages</small>
+                                                </p>
+                                             </div>
+                                          </div>
+                                       </a>
+                                       <!-- list item-->
+                                        <a href="javascript:void(0);" class="list-group-item">
+                                          <div class="media">
+                                             <div class="pull-left">
+                                                <em class="fa fa-diamond fa-2x text-primary"></em>
+                                             </div>
+                                             <div class="media-body clearfix">
+                                                <div class="media-heading">New settings</div>
+                                                <p class="m-0">
+                                                   <small>There are new settings available</small>
+                                                </p>
+                                             </div>
+                                          </div>
+                                        </a>
+                                        <!-- list item-->
+                                        <a href="javascript:void(0);" class="list-group-item">
+                                          <div class="media">
+                                             <div class="pull-left">
+                                                <em class="fa fa-bell-o fa-2x text-danger"></em>
+                                             </div>
+                                             <div class="media-body clearfix">
+                                                <div class="media-heading">Updates</div>
+                                                <p class="m-0">
+                                                   <small>There are
+                                                      <span class="text-primary">2</span> new updates available</small>
+                                                </p>
+                                             </div>
+                                          </div>
+                                        </a>
+                                       <!-- last list item -->
+                                        <a href="javascript:void(0);" class="list-group-item">
+                                          <small>See all notifications</small>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
 
                             <li class="dropdown user-box">
                                 <a href="" class="dropdown-toggle waves-effect waves-light profile " data-toggle="dropdown" aria-expanded="true">
-                                    <img src="<?php echo $params['base_image']; ?>ravi-vendra.png" alt="user-img" class="img-circle user-img">
+                                    <img src="assets/images/users/avatar-1.jpg" alt="user-img" class="img-circle user-img">
                                     <div class="user-status away"><i class="zmdi zmdi-dot-circle"></i></div>
                                 </a>
 
@@ -54,11 +112,11 @@
                         <!-- Navigation Menu-->
                         <ul class="navigation-menu">
                             <li class="active">
-                                <a href="<?php echo @ site_url('backend/dashboard'); ?>"><i class="md md-home"></i> <span> Dashboard </span> </a>
+                                <a href="index.html"><i class="md md-home"></i> <span> Dashboard </span> </a>
                             </li>
 
 
-                            <!-- <li class="has-submenu">
+                            <li class="has-submenu">
                                 <a href="#"><i class="md md-palette "></i><span> Elements </span> </a>
                                 <ul class="submenu">
                                     <li><a href="typography.html">Typography</a></li>
@@ -75,104 +133,88 @@
                             </li>
 
                             <li class="has-submenu">
-                                <a href="#"><i class="md md-palette "></i> <span> Atur Outlet </span> </a>
+                                <a href="#"><i class="md md-invert-colors-on"></i> <span> Components </span> </a>
                                 <ul class="submenu">
-                                    <li class="has-submenu">
-                                        <a href="#">Outlet PLASA TELKOM</a>
-                                        <ul class="submenu">
-                                            <li><a href="<?php echo @ site_url('backend/admin-plasa/manage'); ?>">Manage Outlet</a></li>
-                                        </ul>
-                                    </li>
-
-                                    <li class="has-submenu">
-                                        <a href="#">Terminal PLASA TELKOM</a>
-                                        <ul class="submenu">
-                                            <li><a href="form-elements.html">Manage Terminal</a></li>
-                                        </ul>
-                                    </li>
-
-                                    <li class="has-submenu">
-                                        <a href="#">User PLASA TELKOM</a>
-                                        <ul class="submenu">
-                                            <li><a href="tables.html">Manage User</a></li>
-                                            
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li> -->
-
-                            <li class="has-submenu">
-<<<<<<< HEAD
-                                <a href="#"><i class="md md-redeem"></i> <span> Atur Admin </span> </a>
-=======
-                                <a href="#"><i class="md md-invert-colors-on"></i> <span> Log & Laporan </span> </a>
-                                <ul class="submenu">
-                                    <li><a href="grid.html">Log Browser</a></li>
-                                    <li><a href="portlets.html">Sejarah Transaksi</a></li>
+                                    <li><a href="grid.html">Grid</a></li>
+                                    <li><a href="portlets.html">Portlets</a></li>
+                                    <li><a href="widgets.html">Widgets</a></li>
+                                    <li><a href="nestable-list.html">Nesteble</a></li>
+                                    <li><a href="ui-sliders.html">Sliders </a></li>
+                                    <li><a href="gallery.html">Gallery </a></li>
+                                    <li><a href="pricing.html">Pricing Table </a></li>
+                                    <li><a href="calendar.html">Calendar </a></li>
                                 </ul>
                             </li>
 
                             <li class="has-submenu">
-                                <a href="#"><i class="md md-invert-colors-on"></i> <span> Privatisasi </span> </a>
-                                <ul class="submenu">
-                                    <li><a href="grid.html">Profil-ku</a></li>
-                                    <li><a href="portlets.html">Ubah Password</a></li>
-                                </ul>
-                            </li>
-
-                            <!-- <li class="has-submenu">
                                 <a href="#"><i class="md md-redeem"></i> <span> Other </span> </a>
->>>>>>> 82605b4a56ab3483eea792247272805111c7e603
                                 <ul class="submenu">
                                     <li class="has-submenu">
-                                        <a href="#">Admin PLASA TELKOM </a>
+                                        <a href="#">Icons</a>
                                         <ul class="submenu">
-                                            <li><a href="<?php echo @ site_url('backend/admin-plasa/manage'); ?>">Manage Admin</a></li>
+                                            <li><a href="material-icon.html">Material Design</a></li>
+                                            <li><a href="ion-icons.html">Ion Icons</a></li>
+                                            <li><a href="font-awesome.html">Font awesome</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="has-submenu">
+                                        <a href="#">Forms</a>
+                                        <ul class="submenu">
+                                            <li><a href="form-elements.html">General Elements</a></li>
+                                            <li><a href="form-validation.html">Form Validation</a></li>
+                                            <li><a href="form-advanced.html">Advanced Form</a></li>
+                                            <li><a href="form-wizard.html">Form Wizard</a></li>
+                                            <li><a href="form-editor.html">WYSIWYG Editor</a></li>
+                                            <li><a href="code-editor.html">Code Editors</a></li>
+                                            <li><a href="form-uploads.html">Multiple File Upload</a></li>
+                                            <li><a href="form-xeditable.html">X-editable</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="has-submenu">
+                                        <a href="#">Tables</a>
+                                        <ul class="submenu">
+                                            <li><a href="tables.html">Basic Tables</a></li>
+                                            <li><a href="table-datatable.html">Data Table</a></li>
+                                            <li><a href="tables-editable.html">Editable Table</a></li>
+                                            <li><a href="responsive-table.html">Responsive Table</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="has-submenu">
+                                        <a href="#">Charts</a>
+                                        <ul class="submenu">
+                                            <li><a href="morris-chart.html">Morris Chart</a></li>
+                                            <li><a href="chartjs.html">Chartjs</a></li>
+                                            <li><a href="flot-chart.html">Flot Chart</a></li>
+                                            <li><a href="peity-chart.html">Peity Charts</a></li>
+                                            <li><a href="charts-sparkline.html">Sparkline Charts</a></li>
+                                            <li><a href="chart-radial.html">Radial charts</a></li>
+                                            <li><a href="other-chart.html">Other Chart</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="has-submenu">
+                                        <a href="#">Maps</a>
+                                        <ul class="submenu">
+                                            <li><a href="gmap.html"> Google Map</a></li>
+                                            <li><a href="vector-map.html"> Vector Map</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li class="has-submenu">
+                                        <a href="#">Mail</a>
+                                        <ul class="submenu">
+                                            <li><a href="inbox.html">Inbox</a></li>
+                                            <li><a href="email-compose.html">Compose Mail</a></li>
+                                            <li><a href="email-read.html">View Mail</a></li>
                                         </ul>
                                     </li>
                                 </ul>
                             </li>
 
                             <li class="has-submenu">
-                                <a href="#"><i class="md md-redeem"></i> <span> Atur Outlet </span> </a>
-                                <ul class="submenu">
-                                    <li class="has-submenu">
-                                        <a href="#">Outlet PLASA TELKOM </a>
-                                        <ul class="submenu">
-                                            <li><a href="material-icon.html">Manage Outlet</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="has-submenu">
-                                        <a href="#">Terminal PLASA TELKOM </a>
-                                        <ul class="submenu">
-                                            <li><a href="material-icon.html">Manage Terminal</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="has-submenu">
-                                        <a href="#">User PLASA TELKOM </a>
-                                        <ul class="submenu">
-                                            <li><a href="material-icon.html">Manage User</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            <li class="has-submenu">
-                                <a href="#"><i class="md md-invert-colors-on"></i> <span> Log & Laporan </span> </a>
-                                <ul class="submenu">
-                                    <li><a href="grid.html">Log Browser</a></li>
-                                    <li><a href="portlets.html">Sejarah Transaksi</a></li>                                    
-                                </ul>
-                            </li> -->
-
-<<<<<<< HEAD
-                            <li class="has-submenu">
-                                <a href="#"><i class="md md-invert-colors-on"></i> <span> Privatisasi </span> </a>
-                                <ul class="submenu">
-                                    <li><a href="grid.html">Profile-ku</a></li>
-                                    <li><a href="portlets.html">Ubah Password</a></li>                                    
-=======
-                            <!-- <li class="has-submenu">
                                 <a href="#"><i class="md md-pages"></i><span>Pages </span> </a>
                                 <ul class="submenu megamenu">
                                     <li>
@@ -198,10 +240,8 @@
                                             <li><a href="500.html">500 Error</a></li>
                                         </ul>
                                     </li>
->>>>>>> 82605b4a56ab3483eea792247272805111c7e603
                                 </ul>
-                            </li> -->
-
+                            </li>
 
                         </ul>
                         <!-- End navigation menu  -->
@@ -209,4 +249,3 @@
                 </div>
             </div>
         </header>
-        <!-- End Navigation Bar
