c6be124d59674459af781b2b822b1f876a5f9787
Budi Pratama <boedipratama19@gmail.com>
2016-06-07 16:14:44 +0700

    table final plus form reset

--- application/views/moltran/pages/v-backend-admin-plasa.php
+++ application/views/moltran/pages/v-backend-admin-plasa.php
@@ -16,6 +16,9 @@
                         padding: 3px;
                         box-sizing: border-box;
                     }
+                    #adminPlasa tfoot th:last-child{
+                        display: none;
+                    }
                 </style>
                 <div class="panel">
 
@@ -28,7 +31,7 @@
                             </div>
                         </div>
 
-                        <table id="example" class="display table table-bordered table-striped" cellspacing="0" width="100%">
+                        <table id="adminPlasa" class="display table table-bordered table-striped" cellspacing="0" width="100%">
                             <thead>
                                 <tr>
                                     <th>ID ADMIN</th>
@@ -39,6 +42,15 @@
                                     <th>DI-CREATE</th>
                                     <th>Action</th>
                                 </tr>
+                                <!-- <tr>
+                                    <th>First name</th>
+                                    <th>Last name</th>
+                                    <th>Position</th>
+                                    <th>Office</th>
+                                    <th>Start date</th>
+                                    <th>Salary</th>
+                                    <th>Action</th>
+                                </tr> -->
                             </thead>
                             <tfoot>
                                 <tr>
@@ -48,7 +60,17 @@
                                     <th>STATUS</th>
                                     <th>TANGGAL CREATE</th>
                                     <th>DI-CREATE</th>
+                                    <th>Action</th>
                                 </tr>
+                                <!-- <tr>
+                                    <th>First name</th>
+                                    <th>Last name</th>
+                                    <th>Position</th>
+                                    <th>Office</th>
+                                    <th>Start date</th>
+                                    <th>Salary</th>
+                                    <th>Action</th>
+                                </tr> -->
                             </tfoot>
                         </table>
                     </div>
@@ -66,3 +88,56 @@
             </div>
             <!-- end container -->
 
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.1.4/semantic.min.css" />
+            <!-- form edit -->
+            <div id="con-close-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none">
+                <!-- <form data-toggle="validator" role="form"> -->
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                <h4 class="modal-title">Reset Password</h4>
+                            </div>
+                            <div class="modal-body">
+                                <form id="signupForm" class="ui grid form" style="padding: 10px">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="username" class="control-label">ID ADMIN</label>
+                                                <!-- <input type="text" class="form-control" id="field-2" placeholder="Doe"> -->
+                                                <input id="username" name="username" class="form-control" type="text" readonly="readonly" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="username" class="control-label">Password</label>
+                                                <!-- <input type="text" class="form-control" id="field-2" placeholder="Doe"> -->
+                                                <input id="password" name="password" class="form-control" type="password" placeholder="Password" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label for="username" class="control-label">Confirm Password</label>
+                                                <!-- <input type="text" class="form-control" id="field-2" placeholder="Doe"> -->
+                                                <input id="confirm_password" name="confirm_password" class="form-control" type="password" placeholder="Confirm Password" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-info waves-effect waves-light">Save changes</button>
+                            </div>
+                        </form>   
+                        </div>
+                    </div>
+                <!-- </form> -->
+            </div><!-- /.modal -->
+                                <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"> -->
+                                <!-- <link rel="stylesheet" type="text/css" href="<?php echo $params['base_theme_horizontal'];?>assets/css/docs.css"> -->
+                                <!-- <link rel="stylesheet" type="text/css" href="<?php echo $params['base_theme_horizontal'];?>assets/css/pygments-manni.css"> -->
