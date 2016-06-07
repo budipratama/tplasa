00d11ca1ab33bf3dd1b4f9ae59c6deecae163f25
Budi Pratama <boedipratama19@gmail.com>
2016-06-07 16:10:34 +0700

    tambah skrip validasi dan form search dll

--- application/views/moltran/components/v-backend-script.php
+++ application/views/moltran/components/v-backend-script.php
@@ -50,47 +50,66 @@
                 });
             });
             $(document).ready(function() {
-                $('#example tfoot th').each( function () {
+                // ubah tfooter jadi filter
+                $('#adminPlasa tfoot th').each( function () {
                     var title = $(this).text();
-                    $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-                    // $(this).html( '<input type="text" placeholder="Search" />' );
+                    // alert(title);
+                    if(title == "STATUS")
+                        $(this).html( '<select><option value="">Select Status</option><option value="1">AKTIF</option><option value="0">TIDAK AKTIF</option></select>' );
+                    else
+                        $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
                 } );
-                /*$('#example thead:last-child th').each( function () {
-                    var title = $(this).text();
-                    $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-                    // $(this).html( '<input type="text" placeholder="Search" />' );
-                } );*/
-                var table = $('#example').DataTable( {
+                //  inisialisasi table admin plasa
+                var table = $('#adminPlasa').DataTable( {
                     "processing": true,
                     "serverSide": true,
+                    "sDom":"l",
                     "responsive": true,
                     "ajax": "<?php echo @ site_url('backend/admin-plasa/get-data'); ?>",
-                    /*"columns": [
-                        {
-                            data: null,
-                            defaultContent: '',
-                            className: 'select-checkbox',
-                            orderable: false
-                        },
-                        { data: "EM_USERID" },
-                        { data: "EM_NAME" },
-                        { data: "EM_NAME" },
-                        { data: "EM_STATUS" },
-                        { data: "start_date" },
-                    ],*/
+                    "columnDefs": [ {
+                        "targets": 6,
+                        "data": null,
+                        "defaultContent": "<i class='md md-replay pointer' title='Reset Password' id='reset-password' data-toggle='modal' data-target='#con-close-modal'></i><i class='md md-mode-edit pointer' title='Edit Admin' id='edit-admin-plasa'></i>"
+                        
+                    } ]
                 });
+                // data-toggle="modal" data-target="#con-close-modal"
+                // fungsi filter
                 table.columns().every( function () {
-                    var that = this;             
+                    var that = this;  
+
                     $( 'input', this.footer() ).on( 'keyup change', function () {
-                        console.log(this.value);
                         if ( that.search() !== this.value ) {
-                            console.log("masuk");
+                            that
+                                .search( this.value )
+                                .draw();
+                        }
+                    });
+
+                    $( 'select', this.footer() ).on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+                        that
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                        if ( that.search() !== this.value ) {
                             that
                                 .search( this.value )
                                 .draw();
                         }
                     });
                 } );
+                // event reset password
+                /*$('#adminPlasa tbody').on( 'click', '#reset-password', function () {
+                    var data = table.row( $(this).parents('tr') ).data();
+                    alert("Reset Pass id user"+ data[6]);
+                });*/
+                // event admin plasa
+               /* $('#adminPlasa tbody').on( 'click', '#edit-admin-plasa', function () {
+                    var data = table.row( $(this).parents('tr') ).data();
+                    alert("Edit admin id "+ data[6]);
+                });*/
             });
 
             
@@ -101,3 +120,69 @@
         <script src="<?php echo $params['base_theme_horizontal']; ?>assets/plugins/jquery-datatables-editable/jquery.dataTables.js"></script>
         <script src="<?php echo $params['base_theme_horizontal']; ?>assets/plugins/datatables/dataTables.bootstrap.js"></script>
         <script src="<?php echo $params['base_theme_horizontal']; ?>assets/pages/datatables.editable.init.js"></script>
+        <script src="<?php echo $params['base_theme_horizontal']; ?>assets/js/jquery.validate.js"></script>
+
+        <script type="text/javascript">
+        $.validator.setDefaults( {
+            submitHandler: function () {
+                alert( "submitted!" );
+            }
+        } );
+
+        $( document ).ready( function () {
+            $( "#signupForm" ).validate( {
+                rules: {
+                    /*firstname: "required",
+                    lastname: "required",*/
+                    username: {
+                        required: true,
+                        minlength: 2
+                    },
+                    password: {
+                        required: true,
+                        minlength: 5
+                    },
+                    confirm_password: {
+                        required: true,
+                        minlength: 5,
+                        equalTo: "#password"
+                    },
+                    /*email: {
+                        required: true,
+                        email: true
+                    },
+                    agree: "required"*/
+                },
+                messages: {
+                    // firstname: "Please enter your firstname",
+                    // lastname: "Please enter your lastname",
+                    username: {
+                        required: "Please enter a username",
+                        minlength: "Your username must consist of at least 2 characters"
+                    },
+                    password: {
+                        required: "Please provide a password",
+                        minlength: "Your password must be at least 5 characters long"
+                    },
+                    confirm_password: {
+                        required: "Please provide a password",
+                        minlength: "Your password must be at least 5 characters long",
+                        equalTo: "Please enter the same password as above"
+                    },
+                    // email: "Please enter a valid email address",
+                    // agree: "Please accept our policy"
+                },
+                errorPlacement: function ( error, element ) {
+                    error.addClass( "ui red pointing label transition" );
+                    error.insertAfter( element.parent() );
+                },
+                highlight: function ( element, errorClass, validClass ) {
+                    $( element ).parents( ".row" ).addClass( errorClass );
+                },
+                unhighlight: function (element, errorClass, validClass) {
+                    $( element ).parents( ".row" ).removeClass( errorClass );
+                }
+            } );
+        } );
+    </script>
+
